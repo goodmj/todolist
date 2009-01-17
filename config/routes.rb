061805685eb576cf2dd3838567b3807c7639ee36
Matthew Good <matthew.j.good@gmail.com>
2009-01-17 00:18:42 -0800

    had to use git commit -a to commit had been modified but not picked up by the ctrl-shift-g

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tasks
+
   map.resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090116061809) do
+ActiveRecord::Schema.define(:version => 20090117071734) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
